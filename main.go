237133abf97ffe7ba9f4ac1759920a0bbd651552
serajian.mohsen <serajian.mohsen@gmail.com>
2025-07-21 21:20:34 +0330

    delete comment

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main(){
 	fmt.Println("--------HI from MAIN---------")
 
 	tets:="Hello, World!"
-	// tets:="stress"
+
 	res:=trains.RemoveDuplicates(tets)
 	fmt.Println("RESULT: ",res)
-	
+
 	fmt.Println("********BYE*********")
 }
