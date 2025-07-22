548fae687edaa4399d992b8211aa61e1ff904f50
serajian.mohsen <serajian.mohsen@gmail.com>
2025-07-22 15:26:26 +0330

    exercise no.6 best

--- trains/6.go
+++ trains/6.go
@@ -3,7 +3,8 @@ package trains
 import "strings"
 
 func FlipTheWords(text string) string {
-	arr := strings.Split(text, " ")
+	arr := strings.Fields(text)
+
 	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
 		arr[i], arr[j] = arr[j], arr[i]
 	}
